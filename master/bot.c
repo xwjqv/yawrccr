fe7270f48925b9bed01d9735bf1c23edb125dbcf
xwjqv <35635774+xwjqv@users.noreply.github.com>
2019-04-26 01:01:00 +0200

    fixed bugs in pdrive

--- master/bot.c
+++ master/bot.c
@@ -1,4 +1,7 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #include "bot.h"
 #include "network.h"
@@ -16,16 +19,24 @@ void *botTick(void *arg)
 	bot->tid = pthread_self();
 
 	do{
-		if((bot->speed < 32000 && bot->accelerate > 32000) || (bot->speed > 32000 && bot->accelerate < 32000))
+		if((bot->speed < -32000 && bot->accelerate > 0) || (bot->speed > 32000 && bot->accelerate < 0) || (bot->speed < 32000 && bot->speed > -32000 && bot->accelerate != 0))
 			bot->speed += bot->accelerate * mul;
-		else if ((bot->speed -= bot->speed >> 5) < 1 << 5)
+		else if (abs(bot->speed -= bot->speed >> 5) < 1 << 5)
 			bot->speed = 0;
 	
-		if((bot->direction < 32000 && bot->steer > 32000) || (bot->direction > 32000 && bot->steer < 32000))
+		if((bot->direction < -32000 && bot->steer > 0) || (bot->direction > 32000 && bot->steer < 0) || (bot->direction < 32000 && bot->direction > -32000 && bot->steer !=0))
 			bot->direction += bot->steer * mul;
-		else if ((bot->direction -= bot->direction >> 5) < 1 << 5)
+		else if (abs(bot->direction -= bot->direction >> 5) < 1 << 5)
 			bot->direction = 0;
 
+		/*
+		bot->speed = bot->accelerate * mul;
+		bot->direction = bot->steer * mul;
+		*/	
+
 		sendParam(bot);
+		printf("speed: %d\ndirection: %d\n", bot->speed, bot->direction);
+		printf("accelerate: %d\nsteer: %d\n", bot->accelerate, bot->steer);
+		nanosleep(&(struct timespec){.tv_sec = 0, .tv_nsec = 10000000},NULL);
 	}while(1);
 }
